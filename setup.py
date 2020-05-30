2b63cdb405461451ed0e55f08df429db658fa1c1
teegre <teegre@protonmail.com>
2020-05-30 16:19:30 +0200

    Removed spotify-token module dependency Cannot get an access token from Spotify® by simply submitting username/password anymore. A trick is required to be able to connect.

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='snotify',
-    version='0.4.0',
+    version='0.5.0',
     description='Simple notification tool and playback controller for Spotifyd.',
     author='teegre',
     packages=find_packages(),
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         'spotipy>=2.4.4',
-        'spotify-token>=0.1.0',
     ],
     entry_points={
         'console_scripts': [
